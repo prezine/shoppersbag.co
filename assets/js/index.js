29849c2a6e87163bf771c9ec96540d810b8945bd
Debe <emmanueljoce@gmail.com>
2023-01-12 06:56:32 +0100

    every ui changes implemented

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,48 @@ function showUserCategoryDetail(event){
     console.log(targetId)
     document.querySelector("#"+targetId).classList.add('showing');
 }
+
+// navs and tabs animation
+
+const triggerTabList = document.querySelectorAll('#myTab button')
+triggerTabList.forEach(triggerEl => {
+  const tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', event => {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
+
+// reuseable animation
+
+const txts=document.querySelector(".animate-text").children,
+txtsLen=txts.length;
+
+let index=0;
+const textInTimer=3000,
+ textOutTimer=2800;
+
+function animateText() {
+for(let i=0; i<txtsLen; i++){
+txts[i].classList.remove("text-in","text-out");  
+}
+txts[index].classList.add("text-in");
+
+setTimeout(function(){
+ txts[index].classList.add("text-out");              
+},textOutTimer)
+
+setTimeout(function(){
+
+if(index == txtsLen-1){
+   index=0;
+ }
+else{
+    index++;
+  }
+ animateText();
+},textInTimer); 
+}
+
+window.onload=animateText;
