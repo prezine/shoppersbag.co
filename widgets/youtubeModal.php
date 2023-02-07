bc845274ba5971861573ad7ab3d71de406b5f0b7
Isioma Ekwemuka <Isyekwe@gmail.com>
2023-02-07 13:55:13 +0100

    youtube pop up

---
+++ widgets/youtubeModal.php
@@ -0,0 +1,41 @@
+<section id="youtube_modal" class="font-avenirBook hidden z-200 w-full h-full overflow-y-auto fixed top-0 left-0 flex-col items-center py-10">
+   <div id="" data-modal-close="youtube_modal" class="overlay modal-closer cursor-pointer bg-black/30 w-full h-full z-20 backdrop-blur-sm fixed top-0 left-0 "> </div>
+   <!-- <form class="z-50 bg-brandLightBlue w-ninetyPercent sm:w-sixtyPercent h-fit m-auto lg:w-thirtyFivePercent p-10 rounded-eight flex flex-col gap-2" >
+      <h2 class="text-center font-avenirBlack text-2xl md:text-3xl pb-4 text-black">Crowdfunding Transaction</h2>
+      <section class="flex flex-row pt-7 pb-10 gap-tenPixel justify-center items-center w-full">
+         <div class="px-tenPixel py-1.5 bg-brandDashGreen5x text-text10 sm:text-xs rounded-eight w-fit h-fit text-brandDashGreen4x font-avenirHeavy">
+            <p>Transaction is Protected by Escrow</p>
+         </div>
+         <div class="px-tenPixel py-1.5 bg-brandDashGreen5x text-text10 sm:text-xs rounded-eight w-fit h-fit text-brandDashGreen4x font-avenirHeavy">
+            <p class="amount-contributor">Contributors (0)</p>
+         </div>
+      </section>
+      <section>
+            <h2 class='project-name font-avenirHeavy text-base md:text-xl'>{{ name of project }}</h2>
+            <div class="">
+               <div class="flex flex-row justify-between gap-4 items-center py-2">
+                     <p class="amount-raised text-xs md:text-sm text-brandDashGreen4x">{{ raised }}</p>    
+                     <p class="amount-needed text-xs md:text-sm text-brandDashGray29x">{{ goal }}</p>    
+               </div>
+               <div class="w-full bg-brandDashGrayx rounded-full h-4">
+                  <div class="amount-progress bg-brandDashGreen4x h-4 rounded-full"></div>
+               </div>
+               <div>
+                  <p class="amount-percent pt-2 text-sm"> {{ percent % }} </p>
+               </div>
+            </div>
+      </section>
+      <div class="flex flex-col w-full gap-4 pt-8">
+         <button type="button" class="popActionBTN w-full 2xl:w-fit py-tenPixel text-white bg-brandDashGreen  rounded-fifty flex flex-row gap-2 justify-center" data-payref=""> 
+            <p class="text-xs md:text-base">Go to Crowdpay</p>
+         </button> 
+         <button type="button" class="popTerminateBTN w-full 2xl:w-fit py-tenPixel bg-white rounded-fifty text-brandDashRed flex flex-row gap-2 justify-center">
+            <p class="text-xs md:text-base">Flag this transaction</p>
+         </button>
+      </div>
+   </form> -->
+   <div class="z-50 m-auto h-80 w-full">
+        <iframe class="w-ninetyPercent sm:w-seventyPercent md:w-sixtyPercent lg:w-fortyPercent m-auto max-w-lg xl:max-w-3xl" height="320" src="https://www.youtube.com/embed/825tVcsLE1I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+         <video src=""></video>
+   </div>
+</section>
